00863c1c94bd2e2b4bc7b480e9064c17b7de8e03
Artur Semenas <uncelfuka@gmail.com>
2024-02-19 00:16:07 +0100

    change to spring data jdbc

--- src/main/java/lt/springinaction/tacocloud/repository/IngredientRepository.java
+++ src/main/java/lt/springinaction/tacocloud/repository/IngredientRepository.java
@@ -1,13 +1,14 @@
 package lt.springinaction.tacocloud.repository;
 
 import lt.springinaction.tacocloud.tacos.Ingredient;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface IngredientRepository {
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
-    List<Ingredient> findAll();
-    Optional<Ingredient> findById(String id);
-    Ingredient save(Ingredient ingredient);
+//    List<Ingredient> findAll();
+//    Optional<Ingredient> findById(String id);
+//    Ingredient save(Ingredient ingredient);
 }
--- src/main/java/lt/springinaction/tacocloud/repository/OrderRepository.java
+++ src/main/java/lt/springinaction/tacocloud/repository/OrderRepository.java
@@ -1,8 +1,9 @@
 package lt.springinaction.tacocloud.repository;
 
 import lt.springinaction.tacocloud.tacos.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
-    TacoOrder save(TacoOrder order);
+//    TacoOrder save(TacoOrder order);
 }
