955a78dc9e8a9b5a34e67bd8a6a56a113a19c195
Artur Semenas <uncelfuka@gmail.com>
2024-02-19 23:26:58 +0100

    change to spring data JPA

--- src/main/java/lt/springinaction/tacocloud/tacos/Ingredient.java
+++ src/main/java/lt/springinaction/tacocloud/tacos/Ingredient.java
@@ -1,17 +1,20 @@
 package lt.springinaction.tacocloud.tacos;
 
-import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
 
 @Data
-@Table
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@RequiredArgsConstructor
 public class Ingredient {
 
     @Id
-    private final String id;
-    private final String name;
-    private final Type type;
+    private String id;
+    private String name;
+    private Type type;
 
     public enum Type{
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
--- src/main/java/lt/springinaction/tacocloud/tacos/TacoOrder.java
+++ src/main/java/lt/springinaction/tacocloud/tacos/TacoOrder.java
@@ -1,12 +1,11 @@
 package lt.springinaction.tacocloud.tacos;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -15,13 +14,14 @@ import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
 public class TacoOrder implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date placedAt = new Date();
 
@@ -50,6 +50,7 @@ public class TacoOrder implements Serializable {
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco){
