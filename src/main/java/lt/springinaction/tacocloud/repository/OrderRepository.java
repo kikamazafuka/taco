6aae070b0165fdad19ebb455186d5076d6f3f9ff
Artur Semenas <uncelfuka@gmail.com>
2024-02-21 22:56:08 +0100

    spring data jpa

--- src/main/java/lt/springinaction/tacocloud/repository/OrderRepository.java
+++ src/main/java/lt/springinaction/tacocloud/repository/OrderRepository.java
@@ -1,9 +1,15 @@
 package lt.springinaction.tacocloud.repository;
 
+
 import lt.springinaction.tacocloud.tacos.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
+import java.util.List;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
 //    TacoOrder save(TacoOrder order);
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+    List<TacoOrder> readByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
 }
