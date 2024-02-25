7d28b0b1e43e31c46acd38f03de0f42a3f2e5ec6
Artur Semenas <uncelfuka@gmail.com>
2024-02-25 12:47:54 +0100

    RegistrController, RegistrForm class, registration.html, custom Security filterChain

--- src/main/java/lt/springinaction/tacocloud/security/SecurityConfig.java
+++ src/main/java/lt/springinaction/tacocloud/security/SecurityConfig.java
@@ -5,21 +5,27 @@ import lt.springinaction.tacocloud.tacos.User;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-    
+
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepo){
@@ -29,5 +35,21 @@ public class SecurityConfig {
 
             throw new UsernameNotFoundException("User " + username + " not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/orders", "/design").hasRole("USER")
+                        .requestMatchers("/", "/**").denyAll())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin().loginPage("/login")
+
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/auth/login?error")
+                .permitAll();
+        return httpSecurity.build();
     }
 }
